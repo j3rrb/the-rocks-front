cc7a8d2f8a63482ff9517b2c88407682c2968498
João Emanuel <jao.emanuel@gmail.com>
2023-10-31 22:08:40 -0300

    wip: socket

--- src/components/Dashboard/MonitoringChart.tsx
+++ src/components/Dashboard/MonitoringChart.tsx
@@ -16,8 +16,7 @@ import { useEffect, useMemo } from "react";
 import { listSensorData } from "../../services";
 import { pushData, setData, setLabels } from "../../redux/slices/chart";
 import { RootState } from "../../redux/store";
-import { io } from "socket.io-client";
-import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
+import useWebSocket, { ReadyState } from 'react-use-websocket'
 
 ChartJS.register(
     CategoryScale,
@@ -44,20 +43,42 @@ const options = {
 
 const MonitoringChart = () => {
     const dispatch = useDispatch()
-    useWebSocket('ws://localhost:3100', {
-        onOpen: (event) => { console.log(event) },
-        onMessage(event) {
-            console.log(event);
-        },
-    })
     const { data, labels } = useSelector((state: RootState) => state.chart)
+    // const { lastMessage, readyState } = useWebSocket('ws://localhost:3100/');
+
+    const populateChart = async () => {
+        const [labels, values] = await listSensorData('USIPAV', 1)
+
+        dispatch(setLabels(labels));
+        dispatch(setData(values))
+    }
 
     useEffect(() => {
-        listSensorData('USIPAV', 1).then(([labels, values]) => {
-            dispatch(setLabels(labels));
-            dispatch(setData(values))
-        })
+        const ws = new WebSocket("ws://localhost:3100");
+        ws.onopen = (event) => {
+            console.log({ onopen: event });
+        };
+
+        ws.onmessage = function (this: WebSocket, ev: MessageEvent) {
+            if (this.readyState === 1) {
+                console.log(this, ev);
+            }
+        };
+
+        populateChart()
+
+        return () => {
+            ws.close()
+        }
     }, [])
+
+    // useEffect(() => {
+    //     console.log({ lastMessage });
+    // }, [lastMessage])
+
+    // useEffect(() => {
+    //     console.log({ readyState });
+    // }, [readyState]);
 
     const chartData: ChartData<"line", Record<string, any> | Record<string, any>[], string> = useMemo(() => ({
         labels,
